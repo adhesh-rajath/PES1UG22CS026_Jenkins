abd0b9cb55a4d20c8035f3e76db2afed966d7662
adhesh-rajath <148052178+adhesh-rajath@users.noreply.github.com>
2025-03-13 15:24:28 +0530

    Create program.cpp

---
+++ main/program.cpp
@@ -0,0 +1,10 @@
+no 7.2                                                             program.cpp                                                                      #include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Jenkins!" << endl;
+    return 0;
+}
+
+
+
